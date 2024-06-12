d08c7c2c078638337b323f394547dbe21271068e
Joany61 <rafanomezantsoa617@gmail.com>
2024-06-12 13:31:16 +0300

    small refactoring in connection to DB 2

--- src/services/dbConn.js
+++ src/services/dbConn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
-mongoose.connect(process.env.DB_URI || "mongodb+srv://rafanomezantsoa617:bnIguThJU0hcGUcd@cluster0.kjbiqad.mongodb.net/users?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+mongoose.connect(process.env.DB_URI || "mongodb+srv://rafanomezantsoa617:bnIguThJU0hcGUcd@cluster0.kjbiqad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
   console.log('connection successful')
   //console.log(process.env.DB_URI)
 }).catch((e) => {
